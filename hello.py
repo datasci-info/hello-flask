9ea0dbad0c3835595612dbf6fc3722c821af5d26
c3h3 <c3h3.tw@gmail.com>
2015-09-12 20:42:45 +0800

    render static file

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route('/js/<path:path>')
+def send_js(path):
+  return send_from_directory('static/js/', path)
+
+
 if __name__ == "__main__":
-    app.run()
+  app.debug = True
+  app.run()
