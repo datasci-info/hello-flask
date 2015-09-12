20ad65e49f864ddc0c217db96ee1d92c8c653971
c3h3 <c3h3.tw@gmail.com>
2015-09-12 21:31:12 +0800

    listing message with jinja template for loop

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#TODO: store sample messages into DB
+#TODO: read messages from DB
+sample_messages = ["Message!", "Hello! Flask!", "Flask is awesome!", "new message"]
+
 @app.route('/')
 def guestbook():
-    return render_template('hello.html')
+  #TODO: read messages from DB
+  return render_template('hello.html', messages = sample_messages)
 
 if __name__ == "__main__":
     app.run()
