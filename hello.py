52e2e306cb371814bccd622d2b4f5f9baa958390
c3h3 <c3h3.tw@gmail.com>
2015-09-12 21:27:00 +0800

    simplest fake guestbook: listing messages statically

--- hello.py
+++ hello.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def helloFlask():
-    return "Hello World!"
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name, exampleList= map(str,[4,3,2,1]))
+@app.route('/')
+def guestbook():
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run()
