103e1aafb93796bba16a6980b13c3071b62cfd0d
c3h3 <c3h3.tw@gmail.com>
2015-09-12 20:13:49 +0800

    print request infomation

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+  print request
+  # print dir(request)
+
+  print "request.path", request.path
+  print "request.method", request.method
+  print "request.user_agent", request.user_agent
+  print "request.query_string", request.query_string
+  print "request.cookies", request.cookies
+  print "request.args", request.args
+  print "request.form", request.form
+
+  return "<hr>".join(map(lambda key:"request."+key+" = " + str(request.__dict__[key]),request.__dict__.keys()))
+
 
 if __name__ == "__main__":
     app.run()
